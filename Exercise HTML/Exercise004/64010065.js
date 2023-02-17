7e282bcf84e95a3708daf040ad546725877dc036
KerkaiwanMam <a370957083131@gmail.com>
2023-02-18 00:50:21 +0700

    EX4

---
+++ Exercise HTML/Exercise004/64010065.js
@@ -0,0 +1,8 @@
+
+self.onmessage =(message)=>{
+    let send_time_now = () => {
+        postMessage(new Date().toString());
+    };
+    postMessage(new Date().toString());
+    setInterval(send_time_now, 1000);
+}
